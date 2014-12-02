48cb18851772d81bceba01d4e5d05bb9644c47dd
Marco Vaz <marco.vaz@king.com>
2014-12-02 15:31:20 +0100

    initial commit

---
+++ src/main/java/com/citest/TestIntegration/App.java
@@ -0,0 +1,13 @@
+package com.citest.TestIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
